b96c6173db9b579287b59c291b5fccfc3e9d5759
Tomo Shimizu <tomoshimizu@TomoMac.local>
2022-10-11 18:08:36 +0900

    fix

--- relaxMusic/Utility/Transition.swift
+++ relaxMusic/Utility/Transition.swift
@@ -10,7 +10,7 @@ class Transition {
     static func toPlaySoundView(soundName: NSDataAsset?, sceneNum: Int) {
         let vc = UIApplication.topViewController?.storyboard?.instantiateViewController(withIdentifier: "statusView") as! StatusViewController
         vc.soundName = soundName
-        vc.sceneNum = 1
+        vc.sceneNum = sceneNum
         UIApplication.topViewController?.present(vc, animated: true, completion: nil)
     }
 }
